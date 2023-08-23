36a474dac721290bd8a60d141025cf4475722222
XavierCahaya <223210010@student.unaki.ac.id>
2023-08-23 07:56:31 +0700

    first commit

---
+++ pengenalan.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Pengenalan</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+    <h1><?php echo "Haloo"?></h1>
+    <p>Nama saya Xavier Cahaya Yustisinas, biasa dipanggil Xavier</p>
+    <p>Saya kuliah di Universitas Aki Semarang</p>
+    <p>Saya mengambil jurusan informatika</p>
+    </div>
+    <div class="container">
+    <h1><?php echo "Hobi"?></h1>
+    <p>Game Online</p>
+    <p>Badminton</p>  
+    </div>
+    <div class="container">
+    <h1><?php echo "Makanan Favorit"?></h1>
+    <p>Tongseng</p>
+    <p>Capjay</p>
+</body>
+</html>
